0afa24850e3936604c5442492fe35f063c06ec23
sanesanesane <dohisane1999@gmail.com>
2023-11-02 19:35:58 +0900

    CSS調整

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -5,11 +5,12 @@
         </h2>
     </x-slot>
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
                 <div class="card">
-                    <div class="card-header">カテゴリを追加</div>
+                    <div class="card-header">新しいカテゴリ</div>
 
                     <div class="card-body">
                         <!-- エラーメッセージの表示 -->
@@ -24,11 +25,11 @@
 
                             <!-- カテゴリ名の入力フォーム -->
                             <div class="form-group">
-                                <label for="name">Category Name</label>
+                                <label for="name">カテゴリ名</label>
                                 <input type="text" class="form-control" id="name" name="name" placeholder="Enter category name" required>
                             </div>
 
-                            <button type="submit" class="btn btn-primary">追加する</button>
+                            <button type="submit" class="btn btn-primary">追加</button>
                         </form>
                     </div>
                 </div>
